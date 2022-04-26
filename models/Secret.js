619c20ea1055baa544ed65dc599e71de49e5222f
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-04-26 15:08:00 -0400

    set up schemas for user.js, secret.js, comment.js

---
+++ models/Secret.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const secretSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: [true, 'name cannot be empty :('],
+        maxlength: 500
+    },
+    category: {
+        type: String,
+        required: [true, 'price cannot be empty!']
+    },
+    confessor:{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+// mongoose.model(<mongodb collection name>, our schema)
+const Secret = mongoose.model('Secret', secretSchema);
+
+module.exports = Secret;
