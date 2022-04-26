135d633211ebf19c79245733ee5a63dca3077491
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-04-26 09:46:56 -0400

    Established connection with DB

---
+++ db.connection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const connectionString = process.env.MONGODB_URI
+
+mongoose.connect(connectionString)
+
+mongoose.connection.on('connected', ()=>{
+    console.log('We are connected.')
+
+})
+
+
+mongoose.connection.on('error', ()=>{
+    console.log(`it have an error`, error)
+})
+
+mongoose.connection.on('disconnected', ()=>{
+    console.log('Mongo disconnected')
+})
