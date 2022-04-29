423ad274bcfdf4ea1d47a8fb2771d3d2365be63e
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-04-29 14:47:07 -0400

    switching to main

--- controllers/secrets_controller.js
+++ controllers/secrets_controller.js
@@ -161,19 +161,19 @@ router.delete('/:id', async (req,res, next)=>{
 })
 
 
-router.delete('/:id', async (req,res, next)=>{
-    try {
-        const deletedSecret = await db.Secret.findByIdAndDelete(req.params.id);
-        // delete one secret (req.params.id)
-        // find all comments where secret == req.params.id | delete those as well
-        // const deletedComments = await db.Comment.deleteMany({secret: req.params.id})
-        return res.redirect('/secrets')
-    } catch (error) {
-        console.log(error);
-        req.error = error;
-        return next();
-    }
-})
+// router.delete('/:id', async (req,res, next)=>{
+//     try {
+//         const deletedSecret = await db.Secret.findByIdAndDelete(req.params.id);
+//         // delete one secret (req.params.id)
+//         // find all comments where secret == req.params.id | delete those as well
+//         // const deletedComments = await db.Comment.deleteMany({secret: req.params.id})
+//         return res.redirect('/secrets')
+//     } catch (error) {
+//         console.log(error);
+//         req.error = error;
+//         return next();
+//     }
+// })
 
 // Secrets "update" route - PUT request - update the Secrets array and redirects to show route
 // http://localhost:4000/secrets/0/
