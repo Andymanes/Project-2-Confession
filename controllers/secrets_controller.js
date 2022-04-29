2b3325b652e23385991008d5f548ca7521584b2a
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-04-29 12:12:17 -0400

    delete button and comment function added

--- controllers/secrets_controller.js
+++ controllers/secrets_controller.js
@@ -125,11 +125,7 @@ router.get('/:id/edit', async (req,res, next)=>{
 // Secret "index" route - GET request - displays all secrets
 // http://localhost:4000/secrets
 
-// router.get('/', (req, res) => {
-//     // res.send(secrets)
-//     const context = { secrets }
-//     res.render('index', context)
-// })
+
 
 // Secrets "create" route - POST request -> request body (new product data)
 
@@ -176,6 +172,19 @@ router.delete('/:id', async (req,res, next)=>{
 })
 
 
+router.delete('/:id', async (req,res, next)=>{
+    try {
+        const deletedSecret = await db.Secret.findByIdAndDelete(req.params.id);
+        // delete one secret (req.params.id)
+        // find all comments where secret == req.params.id | delete those as well
+        // const deletedComments = await db.Comment.deleteMany({secret: req.params.id})
+        return res.redirect('/secrets')
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
 
 // Secrets "update" route - PUT request - update the Secrets array and redirects to show route
 // http://localhost:4000/secrets/0/
