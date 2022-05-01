e10a6580cd0cc75691f9d9bdaed3374b6823d025
Maria Jimenez <mtjimenez@gmail.com>
2022-05-01 17:22:51 -0400

    Some Css styling added

---
+++ controllers/images_controller.js
@@ -0,0 +1,51 @@
+// const express = require('express')
+// const router = express.Router()
+// const app = express()
+
+
+
+// let portalImgs = [
+//     "Portal1.JPG",
+//     "Portal2.JPG",
+//     "Portal3.JPG",
+//     "Portal4.JPG",
+//     "Portal5.JPG",
+//     "Portal6.JPG",
+//     "Portal7.JPG",
+//     "Portal8.JPG",
+//     "Portal9.JPG"
+//   ];
+
+//   app.get('/secrets', (req, res) => {
+//     const context = { 
+//         portalImgs : images };
+//     console.log(portalImgs);
+//     res.render('index.ejs', context);
+// });
+
+
+// router.get('./portalimg', (req, res) => {
+//     try {
+//         const image =  portalImgs[i]
+//         const context = { portalImgs[i] : image };
+//         console.log(portalImgs[i]);
+//         res.render('index.ejs', context);
+// } catch (error) {
+//         console.log(error);
+//         req.error = error;
+//        return next();
+//  }
+// });
+
+
+
+
+
+
+
+
+module.exports = router
+
+
+
+
